4bbd99d1d0a4e5d6edc1f4f6c369e14218bed5dd
christt105 <36189169+christt105@users.noreply.github.com>
2019-03-19 19:09:46 +0100

    Image

--- Final_Fantasy_Mystery_World/GUI_Image.cpp
+++ Final_Fantasy_Mystery_World/GUI_Image.cpp
@@ -2,12 +2,5 @@
 #include "p2Defs.h"
 #include "GUI_Image.h"
 
-
-GUI_Image::GUI_Image(int x, int y, UIType type, GUI* parent, Animation* anim, j1Module* callback, SDL_Rect* rect) : GUI(x, y, callback, parent)
-{
-	position.x = x;
-	position.y = y;
-	this->type = type;
-	animation_rect = *rect;
-
-}
+GUI_Image::GUI_Image(const int & pos_x, const int & pos_y, const SDL_Rect & image, GUI * parent, bool inter, bool drag, bool draw, bool clipable)
+	:GUI(IMAGE,pos_x,pos_y,parent,image)	{}
--- Final_Fantasy_Mystery_World/GUI_Image.h
+++ Final_Fantasy_Mystery_World/GUI_Image.h
@@ -6,11 +6,10 @@
 
 class GUI_Image : public GUI
 {
-private:
-	SDL_Rect image;
-
 public:
-	GUI_Image(int x, int y, UIType type, GUI* parent, Animation* anim, j1Module* callback, SDL_Rect* rect = NULL);
+	GUI_Image() : GUI(IMAGE, 0, 0, nullptr, { 0,0,0,0 }) {}
+	GUI_Image(const int& pos_x,const int& pos_y, const SDL_Rect &image, GUI* parent, bool inter = true, bool drag = false, bool draw = true, bool clipable = false);
+	~GUI_Image() {}
 
 };
 #endif //_GUI_IMAGE_H__
