4eef7ea7750dbe5e96746c649428f1da5e370896
Christian Martínez <christt105@gmail.com>
2019-03-23 17:32:26 +0100

    file functions ordered

--- Final_Fantasy_Mystery_World/p2Properties.cpp
+++ Final_Fantasy_Mystery_World/p2Properties.cpp
@@ -1,15 +1,49 @@
 #include "p2Properties.h"
 
 Property::Property() {}
+PropertyString::PropertyString() {}
+PropertyFloat::PropertyFloat() {}
+PropertyBool::PropertyBool() {}
+PropertyInt::PropertyInt() {}
+
+PropertyString::PropertyString(const char *value) : value(value) {}
+PropertyFloat::PropertyFloat(const float &value) : value(value) {}
+PropertyBool::PropertyBool(const bool value) : value(value) {}
+PropertyInt::PropertyInt(const int &value) : value(value) {}
 
 Property::~Property()
 {
 	name.clear();
 }
 
+PropertyString::~PropertyString() {}
+PropertyFloat::~PropertyFloat() {}
+PropertyBool::~PropertyBool() {}
+PropertyInt::~PropertyInt() {}
+
 void Property::SetName(const char * set_name)
 {
 	name.assign(set_name);
+}
+
+void PropertyString::SetValue(const char * val)
+{
+	value.assign(val);
+}
+
+void PropertyFloat::SetValue(const float & val)
+{
+	value = val;
+}
+
+void PropertyBool::SetValue(const bool val)
+{
+	value = val;
+}
+
+void PropertyInt::SetValue(const int & val)
+{
+	value = val;
 }
 
 std::string Property::GetName() const
@@ -17,31 +51,9 @@ std::string Property::GetName() const
 	return name;
 }
 
-PropertyString::PropertyString() {}
-
-PropertyString::PropertyString(const char *value) : value(value) {}
-
-PropertyString::~PropertyString() {}
-
-void PropertyString::SetValue(const char * val)
-{
-	value.assign(val);
-}
-
 std::string PropertyString::GetValue() const
 {
 	return value;
-}
-
-PropertyFloat::PropertyFloat() {}
-
-PropertyFloat::PropertyFloat(const float &value) : value(value) {}
-
-PropertyFloat::~PropertyFloat() {}
-
-void PropertyFloat::SetValue(const float & val)
-{
-	value = val;
 }
 
 float PropertyFloat::GetValue() const
@@ -49,31 +61,9 @@ float PropertyFloat::GetValue() const
 	return value;
 }
 
-PropertyBool::PropertyBool() {}
-
-PropertyBool::PropertyBool(const bool value) : value(value) {}
-
-PropertyBool::~PropertyBool() {}
-
-void PropertyBool::SetValue(const bool val)
-{
-	value = val;
-}
-
 bool PropertyBool::GetValue() const
 {
 	return value;
-}
-
-PropertyInt::PropertyInt() {}
-
-PropertyInt::PropertyInt(const int &value) : value(value) {}
-
-PropertyInt::~PropertyInt() {}
-
-void PropertyInt::SetValue(const int & val)
-{
-	value = val;
 }
 
 int PropertyInt::GetValue() const
