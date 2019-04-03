64ca1c3919d211cbb15f504a63661cb1e97b9dd7
RoperoIvan <36190271+RoperoIvan@users.noreply.github.com>
2019-04-03 17:44:26 +0200

    Camera now stops when the view is out of the map

--- Final_Fantasy_Mystery_World/j1Render.cpp
+++ Final_Fantasy_Mystery_World/j1Render.cpp
@@ -296,17 +296,21 @@ void j1Render::ResetCamera()
 
 void j1Render::LobbyCamera(iPoint playerpos)
 {
-	SDL_Rect cam_frmwork;
-	cam_frmwork.x = (playerpos.x) - (App->win->width / 8);
-	cam_frmwork.y = (playerpos.y)  - (App->win->height / 8);
-	cam_frmwork.h = App->win->height / 4;
-	cam_frmwork.w = App->win->width / 4;
+	SDL_Rect cam_frmwork = { (playerpos.x) - (App->win->width *  0.125),(playerpos.y) - (App->win->height * 0.125), App->win->width * 0.25, App->win->height * 0.25};
 	DrawQuad(cam_frmwork,255,0,255, 80);
 	
-	
-	DrawQuad({-512,0,App->map->data.width*App->map->data.tile_width,App->map->data.height*App->map->data.tile_height },255,0,255,80);
+	SDL_Rect map_rect = { -512,0,App->map->data.width*App->map->data.tile_width,App->map->data.height*App->map->data.tile_height };
+	DrawQuad(map_rect,255,0,255,80);
 
-	camera.x = (-playerpos.x * 3) + (App->win->width / 2);
-	camera.y = (-playerpos.y * 3) + (App->win->height / 2);
+	if (cam_frmwork.x < map_rect.x || cam_frmwork.x + cam_frmwork.w > map_rect.x + map_rect.w || cam_frmwork.y < map_rect.y || cam_frmwork.y + cam_frmwork.w > map_rect.y + map_rect.w)
+	{
+		
+	}
+	else
+	{
+		camera.x = (-playerpos.x * 3) + (App->win->width * 0.5);
+		camera.y = (-playerpos.y * 3) + (App->win->height * 0.5);
+	}
+	
 	
 }
