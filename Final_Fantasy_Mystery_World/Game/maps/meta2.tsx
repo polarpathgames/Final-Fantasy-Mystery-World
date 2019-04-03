7c1267e8a9f5d9a92875693894843d6e2d48eca7
Christian Martínez <christt105@gmail.com>
2019-04-03 20:08:38 +0200

    Deleted unused map files

--- Final_Fantasy_Mystery_World/Game/maps/meta2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="meta2" tilewidth="32" tileheight="16" tilecount="16" columns="4">
- <image source="meta2.png" width="128" height="64"/>
-</tileset>
