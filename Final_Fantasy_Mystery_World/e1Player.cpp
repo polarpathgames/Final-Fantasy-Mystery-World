9b8083f95fb6b5a10e6af723de7484e2ac9ca6a6
RoperoIvan <ivan_ropero98@hotmail.com>
2019-04-07 12:55:26 +0200

    Fix #97

--- Final_Fantasy_Mystery_World/e1Player.cpp
+++ Final_Fantasy_Mystery_World/e1Player.cpp
@@ -556,7 +556,7 @@ void e1Player::PrepareBasicAttack()
 	
 void e1Player::PerformActions(float dt)
 {
-	if (player_input.pressing_V && App->scene->GetMenuState() != StatesMenu::OPTIONS_MENU) {
+	if (player_input.pressing_V && App->scene->GetMenuState() != StatesMenu::OPTIONS_MENU || App->scene->GetMenuState() != StatesMenu::CONTROLS_MENU) {
 		(has_skills) ? DestroySkills() : CreateSkills();
 	}
 	if (state == State::IDLE) {
