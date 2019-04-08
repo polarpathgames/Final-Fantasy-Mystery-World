f096afd4949d56ee23f621578f142a451a90a3a0
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-08 17:30:58 +0200

    tree 3 done

--- Final_Fantasy_Mystery_World/e1StaticEntity.cpp
+++ Final_Fantasy_Mystery_World/e1StaticEntity.cpp
@@ -5,7 +5,7 @@
 
 e1StaticEntity::e1StaticEntity(int x, int y, const char * name):e1Entity(x,y)
 {
-	if (strcmp(name,"flower") == 0) {
+	if (strcmp(name,"flower") == 0) { // finished
 		static_type = e1StaticEntity::Type::FLOWER;
 		frame = { 128,96,32,32 };
 		SetPivot(frame.w*0.5F, frame.h*0.9F);
@@ -16,24 +16,25 @@ e1StaticEntity::e1StaticEntity(int x, int y, const char * name):e1Entity(x,y)
 		frame = { 128,96,32,32 };
 		size.create(frame.w, frame.h);
 	}
-	else if (strcmp(name, "tree2") == 0) {
+	else if (strcmp(name, "tree2") == 0) { // finished
 		static_type = e1StaticEntity::Type::TREE2;
 		frame = { 96,64,32,64 };
 		SetPivot(frame.w*0.5F, frame.h*0.8F);
 		size.create(frame.w, frame.h);
 	}
-	else if (strcmp(name, "tree3") == 0) {
-		static_type = e1StaticEntity::Type::FLOWER;
-		frame = { 128,96,32,32 };
+	else if (strcmp(name, "tree3") == 0) { // finished
+		static_type = e1StaticEntity::Type::TREE3;
+		frame = { 64,64,32,64 };
+		SetPivot(frame.w*0.5F, frame.h*0.9F);
 		size.create(frame.w, frame.h);
 	}
-	else if (strcmp(name, "barrel") == 0) {
+	else if (strcmp(name, "barrel") == 0) { // finished
 		static_type = e1StaticEntity::Type::BARREL;
 		frame = { 32,96,32,32 };
 		SetPivot(frame.w*0.5F, frame.h*0.8F);
 		size.create(frame.w, frame.h);
 	}
-	else if (strcmp(name, "streetlight") == 0) {
+	else if (strcmp(name, "streetlight") == 0) { // finished
 		static_type = e1StaticEntity::Type::STREETLIGHT;
 		frame = { 0,64,32,64 };
 		SetPivot(frame.w*0.5F, frame.h*0.9F);
