38f64c1383b5b0cb6f097e2e0f98784ec986f5de
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-09 00:52:31 +0200

    fix

--- Final_Fantasy_Mystery_World/m1Render.cpp
+++ Final_Fantasy_Mystery_World/m1Render.cpp
@@ -324,24 +324,20 @@ void m1Render::ResetCamera()
 
 void m1Render::SmoothCamera(iPoint playerpos)
 {
-
-
 	BROFILER_CATEGORY("SmoothCamera", Profiler::Color::Aquamarine);
 	if (App->fade_to_black->current_step != App->fade_to_black->fade_to_black) {
 		playerpos.x = (playerpos.x * App->win->GetScale() - camera.w / 2);
 		smoth_position.x -= (playerpos.x + camera.x) / smooth_speed;
 		camera.x = smoth_position.x;
 
-
-
 		playerpos.y = (playerpos.y * App->win->GetScale() - camera.h / 2);
 		smoth_position.y -= (playerpos.y + camera.y) / smooth_speed;
 		camera.y = smoth_position.y;
+	}
 }
 
-
-
-void m1Render::SetCamera(iPoint playerpos) {
+void m1Render::CenterCameraOnPlayer(iPoint playerpos)
+{
 	playerpos.x = (playerpos.x * App->win->GetScale() - camera.w / 2);
 	playerpos.y = (playerpos.y * App->win->GetScale() - camera.h / 2);
 	smoth_position.x -= (playerpos.x + camera.x);
@@ -349,3 +345,7 @@ void m1Render::SetCamera(iPoint playerpos) {
 	smoth_position.y -= (playerpos.y + camera.y);
 	camera.y = smoth_position.y;
 }
+
+
+
+
