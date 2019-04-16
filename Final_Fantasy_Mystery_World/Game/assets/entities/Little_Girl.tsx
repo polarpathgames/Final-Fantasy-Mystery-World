35ed3c889865264c17e516319059682120f6022c
Christian Martínez <christt105@gmail.com>
2019-04-16 17:23:03 +0200

    little girl tsx

--- Final_Fantasy_Mystery_World/Game/assets/entities/Little_Girl.tsx
+++ Final_Fantasy_Mystery_World/Game/assets/entities/Little_Girl.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Little_Girl" tilewidth="32" tileheight="32" tilecount="18" columns="9">
- <image source="../sprites/Little_Girl.png" width="319" height="67"/>
+<tileset version="1.2" tiledversion="1.2.2" name="Little_Girl" tilewidth="18" tileheight="34" tilecount="24" columns="12">
+ <image source="../sprites/Little_Girl.png" width="218" height="72"/>
 </tileset>
