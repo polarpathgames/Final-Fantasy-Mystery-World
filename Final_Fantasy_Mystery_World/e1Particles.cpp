4dd207ca09ae0b8b1050019cea9cce9691599568
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-17 00:21:06 +0200

    arrow

--- Final_Fantasy_Mystery_World/e1Particles.cpp
+++ Final_Fantasy_Mystery_World/e1Particles.cpp
@@ -1,6 +1,9 @@
 #include "e1Particles.h"
 #include "App.h"
 #include "m1Map.h"
+#include "m1EntityManager.h"
+#include "e1DynamicEntity.h"
+
 
 e1Particles::e1Particles(const int & x, const int & y) : e1DynamicEntity(x, y)
 {
@@ -18,9 +21,18 @@ bool e1Particles::PreUpdate()
 
 bool e1Particles::Update(float dt)
 {
+	actual_tile = App->map->WorldToMap(position.x, position.y);
 
-	position.x -= 160 * dt;
-	position.y += 80 * dt;
+	if (NextTileFree(direction)) {
+		position.x += floor(160 * dt);
+		position.y -= floor(80 * dt);
+	}
+	else {
+		to_delete = true;
+	}
+	
+
+	
 
 	return true;
 }
@@ -38,7 +50,8 @@ void e1Particles::SetParticle(const ParticleType & particle_type, const Directio
 	case ParticleType::ARROW:
 		switch (direction) {
 		case Direction::DOWN_LEFT:
-			GoDownLeft.PushBack({ 70,122,85,86 });
+			//GoDownLeft.PushBack({ 16,29,20,20 });
+			GoDownLeft.PushBack({ 37,8,20,20 });
 			size.create(20, 20);
 			current_animation = &GoDownLeft;
 			break;
