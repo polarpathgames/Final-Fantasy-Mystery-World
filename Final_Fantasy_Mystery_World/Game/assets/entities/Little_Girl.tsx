3417b676f31e15491352e7083c140867f0b472e4
RoperoIvan <ivan_ropero98@hotmail.com>
2019-04-16 16:58:53 +0200

    PNG

---
+++ Final_Fantasy_Mystery_World/Game/assets/entities/Little_Girl.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Little_Girl" tilewidth="32" tileheight="32" tilecount="18" columns="9">
+ <image source="../sprites/Little_Girl.png" width="319" height="67"/>
+</tileset>
