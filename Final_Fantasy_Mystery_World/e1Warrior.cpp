b2cb74096b971a8cd9534ed687bbaf9d9b309630
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-16 15:17:34 +0200

    fix

--- Final_Fantasy_Mystery_World/e1Warrior.cpp
+++ Final_Fantasy_Mystery_World/e1Warrior.cpp
@@ -32,6 +32,7 @@ e1Warrior::e1Warrior(const int & x, const int & y) : e1Player(x, y)
 
 	ground = App->tex->Load("assets/sprites/player_pos.png");
 	CenterPlayerInTile();
+	InitStats();
 }
 
 e1Warrior::~e1Warrior()
