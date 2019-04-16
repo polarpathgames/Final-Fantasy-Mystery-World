888cb02b9a0a31f3e349c2e61ecf778b329eb6be
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-16 18:30:34 +0200

    sprites

---
+++ Final_Fantasy_Mystery_World/Game/assets/entities/merche.tsx
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="warrior" tilewidth="28" tileheight="31" tilecount="84" columns="12">
+ <image source="../sprites/Warrior.png" width="336" height="217"/>
+ <tile id="0">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="270"/>
+   <frame tileid="1" duration="270"/>
+   <frame tileid="2" duration="270"/>
+   <frame tileid="1" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="3" duration="270"/>
+   <frame tileid="4" duration="270"/>
+   <frame tileid="5" duration="270"/>
+   <frame tileid="4" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="6" duration="270"/>
+   <frame tileid="7" duration="270"/>
+   <frame tileid="8" duration="270"/>
+   <frame tileid="7" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="9" duration="270"/>
+   <frame tileid="10" duration="270"/>
+   <frame tileid="11" duration="270"/>
+   <frame tileid="10" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="12" duration="270"/>
+   <frame tileid="13" duration="270"/>
+   <frame tileid="14" duration="270"/>
+   <frame tileid="13" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="15" duration="270"/>
+   <frame tileid="16" duration="270"/>
+   <frame tileid="17" duration="270"/>
+   <frame tileid="16" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="18" duration="270"/>
+   <frame tileid="19" duration="270"/>
+   <frame tileid="20" duration="270"/>
+   <frame tileid="19" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="21" duration="270"/>
+   <frame tileid="22" duration="270"/>
+   <frame tileid="23" duration="270"/>
+   <frame tileid="22" duration="270"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="270"/>
+  </animation>
+ </tile>
+</tileset>
