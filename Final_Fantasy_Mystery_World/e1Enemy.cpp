6b04d3d6165fcbc3f0f1f82b16064abcb29c25cd
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-17 19:34:21 +0200

    fix pivot

--- Final_Fantasy_Mystery_World/e1Enemy.cpp
+++ Final_Fantasy_Mystery_World/e1Enemy.cpp
@@ -22,8 +22,6 @@ e1Enemy::e1Enemy(const int &x, const int &y) : e1DynamicEntity(x,y)
 	ground = App->tex->Load("assets/sprites/enemy_pos.png");
 	current_animation = &IdleDownLeft;
 
-	SetPivot(10, 30);
-
 	direction = Direction::DOWN_LEFT;
 	state = State::IDLE;
 	has_turn = false;
