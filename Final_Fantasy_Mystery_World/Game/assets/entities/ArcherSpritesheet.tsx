74e51f5cd7e59042cca0c503bc0342dc5fbb0abb
RoperoIvan <ivan_ropero98@hotmail.com>
2019-04-19 11:24:02 +0200

    New archer tsx

---
+++ Final_Fantasy_Mystery_World/Game/assets/entities/ArcherSpritesheet.tsx
@@ -0,0 +1,292 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="ArcherSpritesheet" tilewidth="18" tileheight="28" tilecount="84" columns="12">
+ <image source="../sprites/ArcherSpritesheet.png" width="216" height="196"/>
+ <tile id="0">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="13" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="16" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="19" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="speed" type="int" value="7"/>
+  </properties>
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="22" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="38" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="41" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="44" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="speed" type="int" value="5"/>
+  </properties>
+  <animation>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="56" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="62" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="62" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="65" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="65" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="65" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="66" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="68" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="71" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="speed" type="int" value="3"/>
+  </properties>
+  <animation>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="71" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
