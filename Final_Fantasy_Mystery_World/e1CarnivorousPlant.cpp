a92c60c01d9a14327d92c35b6111d9a747f48a5f
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-04-22 18:20:57 +0200

    fix

--- Final_Fantasy_Mystery_World/e1CarnivorousPlant.cpp
+++ Final_Fantasy_Mystery_World/e1CarnivorousPlant.cpp
@@ -102,7 +102,9 @@ bool e1CarnivorousPlant::Update(float dt)
 	}
 	if (state == State::DEATH) {
 		if (current_animation->Finished()) {
-			Drop();
+			int drop = App->random.Generate(1, 3);
+			if (drop == 1)
+				Drop();
 			App->audio->PlayFx(App->scene->fx_kill_enemy);
 			App->scene->player->UpdateExperience(stats.experience);
 			App->map->quest_rooms->AddEntityToNotRepeat(original_position);
