884441d99eed9765bc61bcec50c6384d17aec2b1
Christian Martínez <christt105@gmail.com>
2019-04-23 01:09:08 +0200

    pivot and actual tile only blit on debug mode

--- Final_Fantasy_Mystery_World/e1CarnivorousPlant.cpp
+++ Final_Fantasy_Mystery_World/e1CarnivorousPlant.cpp
@@ -113,7 +113,8 @@ bool e1CarnivorousPlant::Update(float dt)
 		}
 	}
 
-	App->render->Blit(ground, App->map->MapToWorld(actual_tile.x, actual_tile.y).x + 1, App->map->MapToWorld(actual_tile.x, actual_tile.y).y - 8, NULL, true);
+	if (App->debug)
+		App->render->Blit(ground, App->map->MapToWorld(actual_tile.x, actual_tile.y).x + 1, App->map->MapToWorld(actual_tile.x, actual_tile.y).y - 8, NULL, true);
 
 	return true;
 }
