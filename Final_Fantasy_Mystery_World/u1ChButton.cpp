bc62b663c09a2a207a0b7f8e8b1e88fbb259fc5d
Christian Martínez <christt105@gmail.com>
2019-04-28 19:01:15 +0200

    improve ChButton

--- Final_Fantasy_Mystery_World/u1ChButton.cpp
+++ Final_Fantasy_Mystery_World/u1ChButton.cpp
@@ -10,85 +10,35 @@
 #include "m1GUI.h"
 #include "m1DialogSystem.h"
 
-u1ChButton::u1ChButton(const int &x, const int &y, const SDL_Rect &idle, const SDL_Rect &hover, const SDL_Rect &push, u1GUI* parent, PlayerType type, bool draw, bool inter, bool drag, bool focus = true)
-	:u1GUI(BUTTON, x, y, parent, idle, draw, inter, drag, focus)
-{
-	hovered_rect = hover;
-	clicked_rect = push;
-	player_type = type;
+u1ChButton::u1ChButton(const int &x, const int &y, const SDL_Rect &idle, const SDL_Rect &hover, const SDL_Rect &push, u1GUI* parent, bool draw, bool inter, bool drag, bool focus = true)
+	:u1Button(x, y, idle, hover, push, parent, draw, inter, drag, focus) {}
 
-
+u1ChButton::~u1ChButton() {
+	elements_to_show.clear();
 }
 
-u1ChButton::~u1ChButton() {}
-
-void u1ChButton::InnerDraw()
+void u1ChButton::UpdateElement()
 {
-	switch (current_state)
-	{
-	case Element_Event::HOVER:
-		App->render->Blit((SDL_Texture*)App->gui->GetAtlas(), draw_offset.x, draw_offset.y, &hovered_rect, false, SDL_FLIP_NONE, 0.0F);
-		PrintImages();
-		break;
-	case Element_Event::CLICKED_DOWN:
-		App->render->Blit((SDL_Texture*)App->gui->GetAtlas(), draw_offset.x, draw_offset.y, &clicked_rect, false, SDL_FLIP_NONE, 0.0F);
-		break;
-	case Element_Event::CLICKED_REPEAT:
-		App->render->Blit((SDL_Texture*)App->gui->GetAtlas(), draw_offset.x, draw_offset.y, &clicked_rect, false, SDL_FLIP_NONE, 0.0F);
-		break;
-	case Element_Event::CLICKED_UP:
-		App->render->Blit((SDL_Texture*)App->gui->GetAtlas(), draw_offset.x, draw_offset.y, &hovered_rect, false, SDL_FLIP_NONE, 0.0F);
-		break;
-	case Element_Event::NONE:
-		App->render->Blit((SDL_Texture*)App->gui->GetAtlas(), draw_offset.x, draw_offset.y, &section, false, SDL_FLIP_NONE, 0.0F);
-	
-		break;
-	default:
-		App->render->Blit((SDL_Texture*)App->gui->GetAtlas(), draw_offset.x, draw_offset.y, &section, false, SDL_FLIP_NONE, 0.0F);
-		break;
+	if (current_state == Element_Event::HOVER) {
+		for (std::list<u1GUI*>::iterator item = elements_to_show.begin(); item != elements_to_show.end(); ++item) {
+			(*item)->drawable = true;
+		}
+	}
+	else {
+		for (std::list<u1GUI*>::iterator item = elements_to_show.begin(); item != elements_to_show.end(); ++item) {
+			(*item)->drawable = false;
+		}
 	}
 }
 
-void u1ChButton::SetRects(const SDL_Rect &std_rect, const SDL_Rect &hl_rect, const SDL_Rect &click_rect)
+void u1ChButton::AddElementsToShow(u1GUI * element)
 {
-	section = std_rect;
-	hovered_rect = hl_rect;
-	clicked_rect = click_rect;
+	elements_to_show.push_back(element);
 }
 
-void u1ChButton::PrintImages()
+void u1ChButton::AddElementsToShow(std::list<u1GUI*> elements)
 {
-	if (App->gui->GetFocus() == App->main_menu->button_warrior)
-	{
-		App->main_menu->warrior_image->drawable = true;
-		App->main_menu->archer_image->drawable = false;
-		App->main_menu->mage_image->drawable = false;
-
-		App->main_menu->warrior_info->drawable = true;
-		App->main_menu->archer_info->drawable = false;
-		App->main_menu->mage_info->drawable = false;
+	for (std::list<u1GUI*>::iterator item = elements.begin(); item != elements.end(); ++item) {
+		elements_to_show.push_back(*item);
 	}
-
-	else if (App->gui->GetFocus() == App->main_menu->button_archer)
-	{
-		App->main_menu->archer_image->drawable = true;
-		App->main_menu->warrior_image->drawable = false;
-		App->main_menu->mage_image->drawable = false;
-
-		App->main_menu->warrior_info->drawable = false;
-		App->main_menu->archer_info->drawable = true;
-		App->main_menu->mage_info->drawable = false;
-	}
-
-	else if (App->gui->GetFocus() == App->main_menu->button_mage)
-	{
-		App->main_menu->mage_image->drawable = true;
-		App->main_menu->warrior_image->drawable = false;
-		App->main_menu->archer_image->drawable = false;
-
-		App->main_menu->warrior_info->drawable = false;
-		App->main_menu->archer_info->drawable = false;
-		App->main_menu->mage_info->drawable = true;
-	}
-
 }
--- Final_Fantasy_Mystery_World/u1ChButton.h
+++ Final_Fantasy_Mystery_World/u1ChButton.h
@@ -1,35 +1,22 @@
 #ifndef __u1ChButton_H__
 #define __u1ChButton_H__
 
-#include "u1UI_Element.h"
-#include "u1Label.h"
+#include "u1Button.h"
 
-class u1Label;
-class u1Image;
-
-enum class PlayerType;
-
-
-class u1ChButton : public u1GUI
+class u1ChButton : public u1Button
 {
 public:
-	u1ChButton() : u1GUI(BUTTON, 0, 0, nullptr, { 0,0,0,0 }) {}
-	u1ChButton(const int &x, const int &y, const SDL_Rect &idle, const SDL_Rect &hover, const SDL_Rect &push, u1GUI* parent, PlayerType type, bool draw, bool inter, bool drag, bool focus);
+	u1ChButton() : u1Button() {}
+	u1ChButton(const int &x, const int &y, const SDL_Rect &idle, const SDL_Rect &hover, const SDL_Rect &push, u1GUI* parent, bool draw, bool inter, bool drag, bool focus);
 	~u1ChButton();
 
-	void InnerDraw();
+	void UpdateElement();
 
-	void SetRects(const SDL_Rect &std_rect, const SDL_Rect &hover_rect, const SDL_Rect &click_rect);
-
-	void PrintImages();
+	void AddElementsToShow(u1GUI* element);
+	void AddElementsToShow(std::list<u1GUI*> elements);
 
 private:
-	//SDL_Rect idle_rect = { 0,0,0,0 }; idle rect is section rect
-	SDL_Rect hovered_rect = { 0,0,0,0 };
-	SDL_Rect clicked_rect = { 0,0,0,0 };
-
-	PlayerType player_type;
-
+	std::list<u1GUI*> elements_to_show;
 };
 
 #endif
