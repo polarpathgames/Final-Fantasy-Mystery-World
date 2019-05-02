e3e62ed7a8aa54f22e7ea5e1fa0782cf456f3e03
RoperoIvan <ivan_ropero98@hotmail.com>
2019-05-02 13:40:23 +0200

    bar now is a circle

--- Final_Fantasy_Mystery_World/u1Bar.cpp
+++ Final_Fantasy_Mystery_World/u1Bar.cpp
@@ -35,7 +35,7 @@ u1Bar::u1Bar(const int &x, const int &y, int max_capacity, UIType type, u1GUI* p
 		current_width = 0;
 		max_width = 33;
 		empty_bar = App->gui->AddImage(x, y, { 1486,2111,33,31 }, nullptr, App->menu_manager->im_skipper, true, false, false, false);
-		filled_bar = App->gui->AddImage(x, y, { 1418,2111,0,31 }, nullptr, empty_bar, true, false, false, false);
+		filled_bar = App->gui->AddImage(x, y, { 1418,2111,0,34 }, nullptr, empty_bar, true, false, false, false);
 	}
 
 	
