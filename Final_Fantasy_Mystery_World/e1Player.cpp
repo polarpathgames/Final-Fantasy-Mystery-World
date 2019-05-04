8cad750594fbdc92a6cf85442f66007431493329
RoperoIvan <ivan_ropero98@hotmail.com>
2019-05-04 19:18:56 +0200

    fix

--- Final_Fantasy_Mystery_World/e1Player.cpp
+++ Final_Fantasy_Mystery_World/e1Player.cpp
@@ -1154,7 +1154,7 @@ void e1Player::Death()
 		App->audio->PlayFx(App->scene->fx_die);
 		App->map->CleanUp();
 		App->entity_manager->DeleteEntitiesNoPlayer();
-		App->menu_manager->DestroyHUD();
+		App->menu_manager->ShowHUD(false);
 		App->menu_manager->CreateGameOver();
 		App->scene->SetMenuState(StatesMenu::DIE_MENU);
 		state = State::MENU;
