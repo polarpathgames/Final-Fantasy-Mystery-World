590baf8091326a0605cdad066557af2a7d6c71bf
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-05-05 12:35:27 +0200

    tsx

---
+++ Final_Fantasy_Mystery_World/Game/assets/entities/Cassio.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="nagas" tilewidth="33" tileheight="40" tilecount="72" columns="12">
+ <image source="../sprites/nagas.png" width="396" height="240"/>
+</tileset>
