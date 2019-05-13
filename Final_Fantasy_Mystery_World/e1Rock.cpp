e5d962f674ecaa314e7e356d7dd73288f6b3a025
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2019-05-14 01:30:45 +0200

    Barrel explodes

--- Final_Fantasy_Mystery_World/e1Rock.cpp
+++ Final_Fantasy_Mystery_World/e1Rock.cpp
@@ -46,7 +46,7 @@ e1Rock::e1Rock(const int &x, const int &y, const char* name) : e1StaticEntity(x,
 	}
 	else if (strcmp(name, "breakable_barrel") == 0) {
 		original_pos = { x,y };
-		frame = { 32,96,32,32 };
+		frame = { 438,122,35,31 };
 		SetPivot(frame.w*0.5F, frame.h*0.8F);
 		size.create(frame.w, frame.h);
 		actual_tile = App->map->WorldToMap(x, y);
@@ -84,18 +84,18 @@ void e1Rock::GetHitted()
 			idle->PushBack({ 955,187,32,37 });
 		}
 		else if (rock_type == RockType::GREY_ROCK) {
-			idle->PushBack({ 795,140,32,37 });
+			idle->PushBack({ 438,122,35,31 });
 			idle->PushBack({ 835,140,32,37 });
 			idle->PushBack({ 875,140,32,37 });
 			idle->PushBack({ 915,140,32,37 });
 			idle->PushBack({ 955,140,32,37 });
 		}
 		else if (rock_type == RockType::BREAKABLE_BARREL) {
-			idle->PushBack({ 795,187,32,37 });
-			idle->PushBack({ 835,187,32,37 });
-			idle->PushBack({ 875,187,32,37 });
-			idle->PushBack({ 915,187,32,37 });
-			idle->PushBack({ 955,187,32,37 });
+			idle->PushBack({ 473,122,35,31 });
+			idle->PushBack({ 508,122,35,31 });
+			idle->PushBack({ 543,122,35,31 });
+			idle->PushBack({ 578,122,35,31 });
+			idle->PushBack({ 613,122,35,31 });
 		}
 		idle->loop = false;
 		idle->speed = 10;
