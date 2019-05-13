ee8a029e6c0c3c5937373eccb1da2ab74df4ab69
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-05-14 01:35:03 +0200

    fix player die

--- Final_Fantasy_Mystery_World/e1Player.cpp
+++ Final_Fantasy_Mystery_World/e1Player.cpp
@@ -1136,6 +1136,7 @@ void e1Player::GetHitted(const int & damage_taken)
 		ReduceLives(damage_taken);
 
 	if (stats.live <= 0) {
+		App->entity_manager->entity_turn = this;
 		state = State::DEATH;
 		ChangeAnimation(direction, state);
 		death_time = SDL_GetTicks();
@@ -1152,14 +1153,12 @@ void e1Player::Death()
 	BROFILER_CATEGORY("Player Death", Profiler::Color::Yellow);
 
 	if (current_animation->Finished() && death_time <= SDL_GetTicks() - 1000) {
-		
 		std::list<u1GUI*> list = App->gui->GetUIList();
 		std::list<u1GUI*>::iterator item = list.begin();
 		for (; item != list.end(); ++item) {
 			if ((*item) != nullptr && (*item)->GetType() == HIT_POINT_LABEL)
 				(*item)->to_delete = true;
 		}
-
 		App->audio->PlayFx(App->scene->fx_die);
 		App->map->CleanUp();
 		App->easing_splines->CleanUp();
