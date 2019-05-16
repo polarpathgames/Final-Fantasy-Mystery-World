c48cc7981bba45ea65c72b3184c56f829da8884d
RoperoIvan <ivan_ropero98@hotmail.com>
2019-05-16 18:57:42 +0200

    fix

--- Final_Fantasy_Mystery_World/Game/assets/entities/Shopkepper.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Shopkepper" tilewidth="16" tileheight="26" tilecount="8" columns="8">
- <image source="../sprites/Shopkepper.png" width="128" height="26"/>
-</tileset>
