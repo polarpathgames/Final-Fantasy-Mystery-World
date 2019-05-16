a23863a6f375b821d4d3ffac73508bc09a53bbe6
Christian Martínez <christt105@gmail.com>
2019-05-16 12:34:47 +0200

    fix frozen animation

--- Final_Fantasy_Mystery_World/Game/assets/entities/Frozen.tsx
+++ Final_Fantasy_Mystery_World/Game/assets/entities/Frozen.tsx
@@ -286,50 +286,58 @@
  </tile>
  <tile id="160">
   <animation>
-   <frame tileid="120" duration="200"/>
-   <frame tileid="121" duration="200"/>
-   <frame tileid="122" duration="200"/>
-   <frame tileid="123" duration="200"/>
-   <frame tileid="124" duration="200"/>
-   <frame tileid="125" duration="200"/>
-   <frame tileid="126" duration="200"/>
-   <frame tileid="127" duration="200"/>
+   <frame tileid="160" duration="200"/>
+   <frame tileid="161" duration="200"/>
+   <frame tileid="162" duration="200"/>
+   <frame tileid="163" duration="200"/>
+   <frame tileid="164" duration="200"/>
+   <frame tileid="165" duration="200"/>
+   <frame tileid="166" duration="200"/>
+   <frame tileid="167" duration="200"/>
+   <frame tileid="168" duration="200"/>
+   <frame tileid="169" duration="200"/>
   </animation>
  </tile>
  <tile id="170">
   <animation>
-   <frame tileid="130" duration="200"/>
-   <frame tileid="131" duration="200"/>
-   <frame tileid="132" duration="200"/>
-   <frame tileid="133" duration="200"/>
-   <frame tileid="134" duration="200"/>
-   <frame tileid="135" duration="200"/>
-   <frame tileid="136" duration="200"/>
-   <frame tileid="137" duration="200"/>
+   <frame tileid="170" duration="200"/>
+   <frame tileid="171" duration="200"/>
+   <frame tileid="172" duration="200"/>
+   <frame tileid="173" duration="200"/>
+   <frame tileid="174" duration="200"/>
+   <frame tileid="175" duration="200"/>
+   <frame tileid="176" duration="200"/>
+   <frame tileid="177" duration="200"/>
+   <frame tileid="178" duration="200"/>
+   <frame tileid="179" duration="200"/>
   </animation>
  </tile>
  <tile id="180">
   <animation>
-   <frame tileid="140" duration="200"/>
-   <frame tileid="141" duration="200"/>
-   <frame tileid="142" duration="200"/>
-   <frame tileid="143" duration="200"/>
-   <frame tileid="144" duration="200"/>
-   <frame tileid="145" duration="200"/>
-   <frame tileid="146" duration="200"/>
-   <frame tileid="147" duration="200"/>
+   <frame tileid="180" duration="200"/>
+   <frame tileid="181" duration="200"/>
+   <frame tileid="182" duration="200"/>
+   <frame tileid="183" duration="200"/>
+   <frame tileid="184" duration="200"/>
+   <frame tileid="185" duration="200"/>
+   <frame tileid="186" duration="200"/>
+   <frame tileid="187" duration="200"/>
+   <frame tileid="188" duration="200"/>
+   <frame tileid="189" duration="200"/>
   </animation>
  </tile>
  <tile id="190">
   <animation>
-   <frame tileid="150" duration="200"/>
-   <frame tileid="151" duration="200"/>
-   <frame tileid="152" duration="200"/>
-   <frame tileid="153" duration="200"/>
-   <frame tileid="154" duration="200"/>
-   <frame tileid="155" duration="200"/>
-   <frame tileid="156" duration="200"/>
-   <frame tileid="157" duration="200"/>
+   <frame tileid="190" duration="200"/>
+   <frame tileid="191" duration="200"/>
+   <frame tileid="192" duration="200"/>
+   <frame tileid="193" duration="200"/>
+   <frame tileid="194" duration="200"/>
+   <frame tileid="195" duration="200"/>
+   <frame tileid="196" duration="200"/>
+   <frame tileid="197" duration="200"/>
+   <frame tileid="198" duration="200"/>
+   <frame tileid="199" duration="200"/>
   </animation>
  </tile>
  <tile id="200">
