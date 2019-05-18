56f66ed840e9b5520f0e7aa33be6315e0e23fada
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-05-18 11:23:09 +0200

    fire ball

--- Final_Fantasy_Mystery_World/e1Particles.cpp
+++ Final_Fantasy_Mystery_World/e1Particles.cpp
@@ -59,6 +59,12 @@ bool e1Particles::Update(float dt)
 	case e1Particles::ParticleType::THUNDERBOLT: {
 		MoveIceStake(dt);
 		break; }
+	case e1Particles::ParticleType::AMAZING_DRAGON_FIRE_BALL: {
+		iPoint pos = App->map->MapToWorld(actual_tile.x, actual_tile.y);
+		SDL_Rect rect{ 2664,4659,120,90 };
+		rect = { 2664,4659,54,40 };
+		App->render->Blit((SDL_Texture*)App->gui->GetAtlas(), pos.x - 10, pos.y - 12, &rect, true);
+		break; }
 	default:
 		break;
 	}
